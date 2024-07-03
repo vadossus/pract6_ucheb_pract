406ba265ca2ac24b509079288325877f2f3b2516
vadossus <157449023+vadossus@users.noreply.github.com>
2024-07-03 11:18:23 +0500

    Delete application_practica2 directory

--- application_practica2/app/src/main/java/com/bignerdranch/android/application_practica2/ui/models/Movie.kt
+++
@@ -1,5 +0,0 @@
-package com.bignerdranch.android.application_practica2.ui.models
-
-import android.media.Image
-
-class Movie (val id: Int, val movie: String, val rating: Double, val image: String, val imdb_url: String)
